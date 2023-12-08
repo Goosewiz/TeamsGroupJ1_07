0bb60ca34bebd69d5ecc396a11e2cc4fef5e0411
Danila <igusevdanila@gmail.com>
2023-12-08 16:39:48 +0400

    task3 without tests

---
+++ src/main/gusev/java23/task2/LovedThings.java
@@ -0,0 +1,100 @@
+package gusev.java23.task2;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class LovedThings {
+    private Map<String, String[]> info = new HashMap<String, String[]>();
+    private Map<String, Integer> allThings = new HashMap<String, Integer>();
+
+    public Map<String, String[]> getInfo() {
+        return info;
+    }
+
+    public void setInfo(Map<String, String[]> info) {
+        this.info = info;
+    }
+    public Map<String, Integer> getAllThings(){
+        return allThings;
+    }
+    public void setAllThings(Map<String, Integer> allThings){
+        this.allThings = allThings;
+    }
+
+    public void addInfo(String newInfo) {
+        String[] temp = newInfo.split(" *: *");
+        String[] lovedThings = temp[1].split(" *, *");
+        Set<String> answer;
+        for (int i = 0; i < lovedThings.length; i++) {
+            if (allThings.keySet().contains(lovedThings[i]))
+                allThings.replace(lovedThings[i], allThings.get(lovedThings[i]) + 1);
+            else
+                allThings.put(lovedThings[i], 1);
+        }
+        if (info.keySet().contains(temp[0])) {
+            info.replace(temp[0], lovedThings);
+        } else {
+            info.put(temp[0], lovedThings);
+        }
+    }
+
+    public void print() {
+        for (String key : info.keySet()) {
+            System.out.println("Ключ " + key);
+            for (int i = 0; i < info.get(key).length; i++) {
+                System.out.print(info.get(key)[i] + " ");
+            }
+            System.out.println(" ");
+        }
+    }
+
+    public Set<String> findThingsAllLove() {
+        Set<String> answer = new HashSet<String>();
+        for (String key : info.keySet()) {
+            if (answer.isEmpty()) {
+                for (int i = 0; i < info.get(key).length; i++) {
+                    answer.add(info.get(key)[i]);
+                }
+            } else {
+                Set<String> temp = new HashSet<String>();
+                for (int i = 0; i < info.get(key).length; i++) {
+                    temp.add(info.get(key)[i]);
+                }
+                answer.retainAll(temp);
+            }
+        }
+        return answer;
+    }
+
+    public Set<String> findAll() {
+        Set<String> answer = new HashSet<String>();
+        for (String key : info.keySet()) {
+            for (int i = 0; i < info.get(key).length; i++) {
+                answer.add(info.get(key)[i]);
+                answer.retainAll(answer);
+            }
+        }
+        return answer;
+    }
+
+    public Set<String> personalPreferences(String key) {
+        Set<String> answer = new HashSet<String>();
+        for (int i = 0; i < info.get(key).length; i++)
+            answer.add(info.get(key)[i]);
+        for (String name : info.keySet()) {
+            if (name != key) {
+                for (int i = 0; i < info.get(name).length; i++) {
+                    if (answer.contains(info.get(name)[i]))
+                        answer.remove(info.get(name)[i]);
+                }
+            }
+        }
+        return answer;
+    }
+
+    public Integer countOfThing(String key){
+        return allThings.get(key);
+    }
+}
---
+++ src/main/gusev/java23/task2/Main.java
@@ -0,0 +1,47 @@
+package gusev.java23.task2;
+
+import java.util.Scanner;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        LovedThings lt = new LovedThings();
+        System.out.println("Стартуем");
+        String scan = sc.nextLine();
+        while (scan != "") {
+            lt.addInfo(scan);
+            scan = sc.nextLine();
+        }
+        sc.close();
+        lt.print();
+        Set<String> answer = lt.findThingsAllLove();
+        System.out.print("Все любят - ");
+        for (String item : answer) {
+            System.out.print(item + " ");
+        }
+        System.out.println("");
+        answer = lt.findAll();
+        System.out.print("Все любимые вещи - ");
+        for (String item : answer) {
+            System.out.print(item + " ");
+        }
+        System.out.println("");
+        System.out.print("Персональные предпочтения - ");
+        for (String key : lt.getInfo().keySet()) {
+            System.out.print(key + " ");
+            answer = lt.personalPreferences(key);
+            for (String item : answer) {
+                System.out.print(item + " ");
+            }
+            System.out.println("");
+        }
+        System.out.print("Сколько раз вещь была записана в любимые - ");
+        for (String key : lt.getAllThings().keySet()){
+            System.out.print(key + " ");
+            Integer answ = lt.countOfThing(key);
+            System.out.print(answ);
+            System.out.println("");
+        }
+    }
+}
